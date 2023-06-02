63933d72adf155e58c3ff984d1a0e28691e9f933
raulszpak <raulszpak@gmail.com>
2023-06-02 09:58:03 -0300

    adicionando token jwt em getstudantbyclassid

--- src/Backend/routes/student.js
+++ src/Backend/routes/student.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const tokenValidation = require("../middlewares/tokenValidation")
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const router = express.Router();
 
 const studentController = require("../controllers/studentController");
 
-router.get("/", studentController.getAllStudent);
-router.get("/class_id", urlencodedParser, studentController.getStudentByClassId)
-router.post("/", urlencodedParser, studentController.postStudent);
+router.get("/class/:class_id", tokenValidation, urlencodedParser, studentController.getStudentByClassId)
 router.get("/:student_id", studentController.getStudent);
+router.get("/", studentController.getAllStudent);
+router.post("/", urlencodedParser, studentController.postStudent);
 router.put("/:student_id", urlencodedParser, studentController.putStudent);
 router.delete("/:student_id",urlencodedParser,studentController.removeStudent);
 
