4583a50520b649d89f40edc29f8df69524018c4f
Vitto-Mazeto <Vitto.Mazeto@sou.inteli.edu.br>
2023-06-04 12:31:18 -0300

    funcionou! 🎉🎉🎉😎

--- src/Frontend/scripts/classes.js
+++ src/Frontend/scripts/classes.js
@@ -50,58 +50,56 @@ function getTokenFromLocalStorage() {
   
   // Função para criar as divs dinamicamente com base nas porcentagens
   function createDivs(classes) {
-    const container = document.querySelector('.students');
-  
+    const container = document.querySelector('#container');
+
     classes.forEach(async (classData) => {
-      const { id, class_title, teacher_id } = classData;
-      try {
-        const averages = await fetchAverageClassGrades(id);
-  
-        const div = document.createElement('div');
-        div.className = 'card';
-  
-        const h2 = document.createElement('h2');
-        h2.className = 'student-name';
-        h2.textContent = class_title;
-        div.appendChild(h2);
-  
-        // Aqui você pode criar as divs internas com base nas médias das notas
-        // e definir o tamanho delas com base na porcentagem do pai
-        // Exemplo:
-        const competencyNames = [
-          'Eu, o outro e nós',
-          'Corpo, gestos e movimentos',
-          'Traços, sons, cores e formas',
-          'Escuta, fala, pensamento e imaginação',
-          'Espaços, tempos, quantidades, relações e transformações'
-        ];
-        const competencyColors = ['yellow', 'blue', 'red', 'green', 'grey'];
-  
-        competencyNames.forEach((competency, index) => {
-          const competencyDiv = document.createElement('div');
-          competencyDiv.className = 'competency';
-  
-          const progressBarDiv = document.createElement('div');
-          progressBarDiv.className = `${competencyColors[index]}-100 progress-bar-default`;
-  
-          const percentage = averages[`average_grade${index + 1}`] || 0; // Se a média for nula, considera 0
-  
-          progressBarDiv.style.width = `${percentage}% * 10`;
-          competencyDiv.appendChild(progressBarDiv);
-  
-          const p = document.createElement('p');
-          p.textContent = competency;
-          competencyDiv.appendChild(p);
-  
-          div.appendChild(competencyDiv);
-        });
-  
-        container.appendChild(div);
-      } catch (error) {
-        console.error('Error creating divs:', error);
-      }
+        const { id, class_title, teacher_id } = classData;
+        try {
+            const averages = await fetchAverageClassGrades(id);
+
+            const div = document.createElement('div');
+            div.className = 'card';
+
+            const h2 = document.createElement('h2');
+            h2.className = 'class-name';
+            h2.textContent = class_title;
+            div.appendChild(h2);
+
+            const competencyNames = [
+                'Eu, o outro e nós:',
+                'Corpo, gestos e movimentos:',
+                'Traços, sons, cores e formas:',
+                'Escuta, fala, pensamento e imaginação:',
+                'Espaços, tempos, quantidades, relações e transformações:'
+            ];
+            const competencyColors = ['yellow', 'blue', 'red', 'green', 'grey'];
+
+            competencyNames.forEach((competency, index) => {
+                const competencyDiv = document.createElement('div');
+                competencyDiv.className = `competency ${competencyColors[index]}-50`;
+
+                const p = document.createElement('p');
+                p.textContent = competency;
+
+                const progressBarDiv = document.createElement('div');
+                progressBarDiv.className = `${competencyColors[index]}-100 progress-bar-default`;
+
+                const percentage = averages[`average_grade${index + 1}`] || 0;
+                progressBarDiv.style.width = `${percentage * 10}%`;
+                competencyDiv.appendChild(progressBarDiv);
+
+                div.appendChild(p);
+                div.appendChild(competencyDiv);
+            });
+
+            container.appendChild(div);
+        } catch (error) {
+            console.error('Error creating divs:', error);
+        }
     });
-  }
+}
+
+
   
   // Função principal para obter as classes e criar as divs
   async function initialize() {
