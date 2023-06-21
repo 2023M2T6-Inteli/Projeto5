6303f31313a9717e2b57426428d4620f1c5d0fca
Gabriel Farias <gabriel.alves@sou.inteli.edu.br>
2023-06-21 14:58:41 -0300

    feat: route / to return index.html

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const databaseConnection = require("./middlewares/databaseConnection")
+const databaseConnection = require("./middlewares/databaseConnection");
 const hostname = "127.0.0.1";
 const app = express();
+const path = require("path");
 const dotenv = require("dotenv");
-dotenv.config({path: __dirname + "/.env"});
+const cors = require("cors");
+
+dotenv.config({ path: __dirname + "/.env" });
 app.use(express.json());
-const cors = require('cors');
 app.use(cors());
+app.use(express.static(path.join(__dirname, "/../Frontend")));
 
 const PORT = process.env.PORT;
 
-const registerRoute = require("./routes/register")
+const registerRoute = require("./routes/register");
 const loginRoute = require("./routes/login");
 const teacherRoute = require("./routes/teacher");
 const classRoute = require("./routes/class");
@@ -21,6 +24,10 @@ const studentNotesRoute = require("./routes/studentNotes");
 const studentGradesRoute = require("./routes/studentGrades");
 
 app.use(databaseConnection);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/index.html"));
+});
 
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
@@ -32,5 +39,5 @@ app.use("/studentNotes", studentNotesRoute);
 app.use("/studentGrades", studentGradesRoute);
 
 app.listen(PORT, hostname, () => {
-  console.log(`Servidor rodando em http://${hostname}:${PORT}/`);
+    console.log(`Servidor rodando em http://${hostname}:${PORT}/`);
 });
